647ff370a60b0f8254f972619b8026e52910ba79
Parker Ennis <rpennis@ncsu.edu>
2015-03-08 16:51:34 -0400

    #shipit, #shipit real good without weather showing

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -4,6 +4,7 @@ class Weather
   end
 
   def current_weather(state_code, city)
+    @city = city
     @weather = HTTParty.get("http://api.wunderground.com/api/#{ENV['WEATHER_TOKEN']}/conditions/q/#{state_code}/#{city}.json")
     @weather["current_observation"]["weather"]
     sports_baby
@@ -15,9 +16,14 @@ class Weather
     @sport = HTTParty.get("https://api.sportsdatallc.org/ncaamb-t3/games/#{@date}/schedule.xml?api_key=#{ENV['SPORTS_TOKEN']
     }")
     venues = @sport["league"]["daily_schedule"]["games"]["game"]
-    venues.each do |v|
-      v[:city]
+    cities = []
+    if venues.each do |v|
+      if v["venue"]["city"].downcase == @city.downcase.gsub( "_", " ")
+        cities << v
+      end
     end
+  end
+    return cities
   end
 
   def date
